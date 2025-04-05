9e0f5592688eee7d544e49d4760862db4e6de0b4
RayanSUK <sukkarie.rayan@gmail.com>
2025-04-05 12:54:55 +0200

    Enregistrement du bluePrint Api

--- src/archilog/views/__init__.py
+++ src/archilog/views/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from archilog.views.web_ui import web_ui
+from archilog.views.api import api,spec
 def create_app():
     app = Flask(__name__)
 
@@ -8,4 +9,8 @@ def create_app():
 
     #Enregistrer les bluePRints
     app.register_blueprint(web_ui)
+    app.register_blueprint(api) #nouveau
+
+    spec.register(app) #important pour Swagger
+
     return app
