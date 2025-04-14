bf00e46d683bb4017b6b23d821707eda18396e5e
RayanSUK <162138316+RayanSUK@users.noreply.github.com>
2025-04-14 13:59:46 +0200

    Update api.py pour import csv

--- src/archilog/views/api.py
+++ src/archilog/views/api.py
@@ -3,7 +3,7 @@ from urllib import request
 from uuid import UUID
 
 from flask import Flask, Blueprint, jsonify #Prérequis
-from spectree import SpecTree, SecurityScheme
+from spectree import SpecTree, SecurityScheme , BaseFile
 from pydantic import BaseModel, Field #Utilisation
 from flask_httpauth import HTTPTokenAuth
 import archilog.models as models
@@ -90,12 +90,19 @@ def delete_entry(id):
     return {"message": "Suppression réussie"}
 
 
+
+class File(BaseModel):
+    uid: str
+    file: BaseFile
+
+
 @api.route("/user/import", methods=["POST"])
 @auth.login_required
 @spec.validate(tags=["user"])
 def import_csv():
     file_path = request.json.get("file_path")
     models.import_from_csv(file_path)
+    
     return {"message": "Importation réussie depuis le fichier CSV"}
 
 
